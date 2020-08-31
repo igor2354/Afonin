45957895be2fafdf2650778d63af7fe5ddedd4b7
igor2354 <66739722+igor2354@users.noreply.github.com>
2020-08-31 18:57:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
+function popupTeam(el) {
+  $(".popup__university").text(el.find($(".item-team__info-edu")).text());
+  $(".popup__spec-dicript").text(el.find($(".item-team__info-spec")).text());
+  $(".popup__contact-email").text(el.find($(".item-team__info-contact")).text());
+  $(".popup__name").text(el.find($(".item-team__name")).text());
+  $(".popup__post").text(el.find($(".item-team__info-post")).text());
+  $(".popup__image")
+    .find("img")
+    .attr("src", el.find($("image")).attr("xlink:href"));
+}
+
+function popupSupport(callback) {
+  $(".popup").addClass("open");
+  $("body").addClass("lock");
+  $(".popup-2").find($(".support__group")).remove();
+  $(".popup-2").find($(".support__list-popup")).remove();
+  callback();
+}
+
+function videoName(selector) {
+  $("#tab-2")
+    .find($(".publication-2:nth-child(4)"))
+    .find(selector)
+    .text($("#tab-2").find($(".publication-2:nth-child(1)")).find(selector).text());
+}
+
+function pauseVideo(element) {
+  const video = $(".publication-2__video").find("video");
+  $.each(video, function (index, val) {
+    if (element[0] != val) {
+      val.pause();
+    }
+  });
+}
+
 $(document).ready(function () {
   $(window).on("scroll", function () {
     let scrolled = $(this).scrollTop();
+    let srcollElem = [".header", ".juridical", ".team", ".support", ".news", ".article"];
     if (scrolled > 141) {
-      $(".header").addClass("scrolled");
-      $(".juridical").addClass("scrolled");
-      $(".team").addClass("scrolled");
+      $.each(srcollElem, function (index, val) {
+        $(val).addClass("scrolled");
+      });
     }
     if (scrolled <= 141) {
-      $(".header").removeClass("scrolled");
-      $(".juridical").removeClass("scrolled");
-      $(".team").removeClass("scrolled");
+      $.each(srcollElem, function (index, val) {
+        $(val).removeClass("scrolled");
+      });
     }
   });
 
@@ -37,67 +73,37 @@ $(document).ready(function () {
     $($(this).attr("href")).addClass("active");
   });
   $(".tabs__button:first").click();
+
   //=====================IMG TO BACKGROUND CSS================================
   $.each($(".ibg"), function (index, val) {
     if ($(this).find("img").length > 0) {
-      $(this).css(
-        "background",
-        'url("' +
-          $(this).find("img").attr("src") +
-          '") no-repeat center / cover'
-      );
+      $(this).css("background", 'url("' + $(this).find("img").attr("src") + '") no-repeat center / cover');
     }
   });
 
   //==================PLAY VIDEO==============================================
 
-  $("#tab-2")
-    .find($(".publication:nth-child(4)"))
-    .find(".publication__name")
-    .text(
-      $("#tab-2")
-        .find($(".publication:nth-child(1)"))
-        .find(".publication__name")
-        .text()
-    );
+  videoName(".publication-2__name");
+  videoName(".publication-2__text");
 
-  $("#tab-2")
-    .find($(".publication:nth-child(4)"))
-    .find(".publication__text")
-    .text(
-      $("#tab-2")
-        .find($(".publication:nth-child(1)"))
-        .find(".publication__text")
-        .text()
-    );
-
-  $(".publication__video-prev").on("click", function () {
-    if ($(this).parents(".publication__video").find("video").get(0).paused) {
-      $(this).parents(".publication__video").find("video").get(0).play();
+  $(".publication-2__video-prev").on("click", function () {
+    if ($(this).parents(".publication-2__video").find("video").get(0).paused) {
+      $(this).parents(".publication-2__video").find("video").get(0).play();
       $(this).css({ display: "none" });
     }
   });
 
-  function pauseVideo(element) {
-    const video = $(".publication__video").find("video");
-    $.each(video, function (index, val) {
-      if (element[0] != val) {
-        val.pause();
-      }
-    });
-  }
-
-  $(".publication__video").on("click", function (e) {
-    if ($(this)[0] != $(".publication__video:first")[0]) {
-      $(".publication__video:first").parent().before($(this).parent());
+  $(".publication-2__video").on("click", function (e) {
+    if ($(this)[0] != $(".publication-2__video:first")[0]) {
+      $(".publication-2__video:first").parent().before($(this).parent());
       $("#tab-2")
-        .find($(".publication:nth-child(4)"))
-        .find(".publication__name")
-        .text($(this).parent().find(".publication__name").text());
+        .find($(".publication-2:nth-child(4)"))
+        .find(".publication-2__name")
+        .text($(this).parent().find(".publication-2__name").text());
       $("#tab-2")
-        .find($(".publication:nth-child(4)"))
-        .find(".publication__text")
-        .text($(this).parent().find(".publication__text").text());
+        .find($(".publication-2:nth-child(4)"))
+        .find(".publication-2__text")
+        .text($(this).parent().find(".publication-2__text").text());
 
       pauseVideo($(this).find("video"));
     }
@@ -141,40 +147,36 @@ $(document).ready(function () {
   $(".scrollbar-rail").scrollbar();
 
   //=================POPUP========================
-  $.each($(".team__wrap"), function (index, val) {
+
+  $.each($(".popup-open"), function (index, val) {
     $(val).on("click", function () {
       $(".popup").addClass("open");
       $("body").addClass("lock");
-      $(".popup__university").text($(this).find($(".team__info-edu")).text());
-      $(".popup__spec-dicript").text(
-        $(this).find($(".team__info-spec")).text()
-      );
-      $(".popup__contact-email").text(
-        $(this).find($(".team__info-contact")).text()
-      );
-      $(".popup__name").text($(this).find($(".team__name")).text());
-      $(".popup__post").text($(this).find($(".team__info-post")).text());
-      $(".popup__image")
-        .find("img")
-        .attr("src", $(this).find($("image")).attr("xlink:href"));
+
+      if ($(this).parents(".team__wrap")) {
+        popupTeam($(val));
+      }
+
+      if ($(this).parents(".support__wrap")) {
+        popupSupport(function () {
+          $(val).parent().find(".support__group").clone().prependTo(".popup__content");
+          $(val).parent().find(".support__list-popup").clone().appendTo(".popup__content");
+        });
+      }
     });
-  });
-  $.each($(".popup__close"), function (index, val) {
-    $(val).on("click", function () {
-      $(".popup").removeClass("open");
-      $("body").removeClass("lock");
-    });
-  });
-  $(".popup__body").on("click", function (e) {
-    if (e.target === $(".popup__body")[0]) {
-      $(".popup").removeClass("open");
-      $("body").removeClass("lock");
-    }
-    e.stopPropagation();
   });
 
-  //================HOVER POPUP=========================
-  $.each($(".team__wrap"), function (index, val) {
+  $.each($(".popup-close"), function (index, val) {
+    $(val).on("click", function (e) {
+      if (e.target === $(".popup__body")[0] || e.target === $(".popup__close")[0]) {
+        $(".popup").removeClass("open");
+        $("body").removeClass("lock");
+      }
+    });
+  });
+
+  //================HOVER TEAM=========================
+  $.each($(".item-team__wrap"), function (index, val) {
     $(val).hover(
       function () {
         $(this).find($("circle")).attr({ r: "100%" });
